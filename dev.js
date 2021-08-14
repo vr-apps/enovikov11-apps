f8e9e77b6b93560f1138a2a7f8539a78583718d7
Evgenii Novikov <enovikov11@yandex.ru>
2021-08-15 00:59:56 +0300

    reloading

--- dev.js
+++ dev.js
@@ -1,6 +1,8 @@
 import * as THREE from '/three.js/build/three.module.js';
 import { STLLoader } from '/three.js/examples/jsm/loaders/STLLoader.js';
 
+// mesh.position ; mesh.quaternion ; .copy
+
 export async function init() {
     const dartGeomentry = await new Promise(res => { new STLLoader().load('/enovikov11-apps/dart.stl', res); }),
         dartMatrial = new THREE.MeshStandardMaterial({ flatShading: true }),
@@ -11,33 +13,42 @@ export async function init() {
 
     let lastUpdatedAt = Date.now(), flyingDarts = [];
 
-    camera.position.set(0, 1.6, 3);
+    camera.position.set(0, 0, 5);
 
     scene.background = new THREE.Color(0x222222);
 
-    scene.add(new THREE.GridHelper(10, 20, 0x111111, 0x111111));
+    scene.add(new THREE.GridHelper(20, 20, 0x111111, 0x111111));
     scene.add(new THREE.HemisphereLight(0x888877, 0x777788));
 
     const light = new THREE.DirectionalLight(0xffffff, 0.5);
-    light.position.set(0, 4, 0);
+    light.position.set(0, 7, 0);
     scene.add(light);
 
     renderer.outputEncoding = THREE.sRGBEncoding;
     renderer.xr.enabled = true;
 
     [renderer.xr.getController(0), renderer.xr.getController(1)].forEach(controller => {
-        controller.addEventListener('selectstart', () => {
-            const newDart = dart.clone();
-
-            newDart.position.copy(controller.position);
-            newDart.rotation.copy(controller.rotation);
-
-            flyingDarts.push({ mesh: newDart, counter: 0 });
-            scene.add(newDart);
-        });
-
-        controller.add(dart.clone());
         scene.add(controller);
+
+        let currentDart = dart.clone(), isReloading = false;
+        controller.add(currentDart);
+
+        controller.addEventListener('select', () => {
+            if (isReloading) { return; }
+
+            controller.remove(currentDart);
+            scene.add(currentDart);
+            currentDart.position.copy(controller.position);
+            currentDart.quaternion.copy(controller.quaternion);
+            flyingDarts.push({ mesh: currentDart, counter: 0 });
+            isReloading = true;
+
+            setTimeout(() => {
+                isReloading = false;
+                currentDart = dart.clone();
+                controller.add(currentDart);
+            }, 500);
+        });
     });
 
     renderer.setAnimationLoop(() => {
